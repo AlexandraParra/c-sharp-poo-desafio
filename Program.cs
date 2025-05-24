d68e014319a8013870682623e1d7b430709e1a14
Alexandra Parra <alexandraparrarz@gmail.com>
2025-05-24 16:40:53 -0300

    Adicionando herança de Smartphone à classe Iphone e implementando o método InstalarAplicativo()

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("=== Nokia ===");
 Nokia nokia = new Nokia("12345", "Modelo 1", "111111111", 8);
 nokia.Ligar();
 nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n=== Iphone ===");
+Iphone iphone = new Iphone("4987", "Modelo 2", "222222222", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
